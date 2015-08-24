fc5d4d0199167e81d34dcecb2a58a8634ddfd0d3
hawen <512309453@qq.com>
2015-08-24 15:29:18 +0800

    dev

---
+++ task3/Student.py
@@ -0,0 +1,7 @@
+__author__ = 'Administrator'
+class Student(object):
+  def __init__(self,name,birthday,phone,email):
+     self.name=name
+     self.birthday=birthday
+     self.phone=phone
+     self.email=email
